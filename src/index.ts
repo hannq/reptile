ba71f5a57c6ad190c64b1bfc992be2f3f1c512be
hannq <1094489295@qq.com>
2020-04-29 01:31:22 +0800

    add demo

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
 /*
- * @Author: hannq 
- * @Date: 2020-04-29 00:09:34 
- * @Last Modified by:   hannq 
- * @Last Modified time: 2020-04-29 00:09:34 
+ * @Author: hannq
+ * @Date: 2020-04-29 00:09:34
+ * @Last Modified by: hannq
+ * @Last Modified time: 2020-04-29 01:14:11
  * @desc 主入口文件
  */
+
+import puppeteer from 'puppeteer';
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://www.baidu.com');
+  await page.screenshot({path: 'screenshot.png'});
+  await browser.close();
+})();
