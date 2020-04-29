98439738b2e4b1c26eb9418f5c48f7a65cddee83
韩念琪 <hannianqi@shein.com>
2020-04-29 13:48:24 +0800

    增加demo

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,37 @@
  * @Author: hannq
  * @Date: 2020-04-29 00:09:34
  * @Last Modified by: hannq
- * @Last Modified time: 2020-04-29 01:14:11
+ * @Last Modified time: 2020-04-29 12:34:52
  * @desc 主入口文件
  */
 
 import puppeteer from 'puppeteer';
+import path from 'path';
+import micromatch from 'micromatch';
+import fse from 'fs-extra';
+import { Tabletojson as tabletojson } from 'tabletojson';
+import xlsx from 'node-xlsx';
 
-(async () => {
+const DATA_CACHE_PATH = path.join(__dirname, './data');
+
+;(async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://www.baidu.com');
-  await page.screenshot({path: 'screenshot.png'});
+  await page.goto('http://wztjj.wenzhou.gov.cn/art/2019/12/3/art_1468714_40733753.html');
+  const tableHtml = await page.$eval('table#c', (ele: HTMLElement) => ele.innerHTML);
   await browser.close();
+  const result: {[x: number]: string}[][] = tabletojson.convert(tableHtml);
+  const resultNew = result
+    .map(item => (item.map(v => (console.log(v), Object.keys(v).reduce((acc, k) => (acc[k] = v[k], acc), [])))))
+    .reduce((acc, v) => [...acc, ...v])
+  await fse.ensureDir(DATA_CACHE_PATH);
+  // const ws = fse.createWriteStream(path.join(DATA_CACHE_PATH, 'table.json') )
+  // ws.write(JSON.stringify(resultNew));
+
+  const buffer = xlsx.build([{ name: 'myExcel', data: resultNew }], {  }); // Returns a buffer
+  const ws = fse.createWriteStream(path.join(DATA_CACHE_PATH, 'table.xlsx') )
+  ws.write(buffer);
+
+  // await page.screenshot({path: 'screenshot.png'});
+  // await browser.close();
 })();
