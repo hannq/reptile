c6165c88e322760602b4bdf5af8ba509b7218f06
hannq <1094489295@qq.com>
2020-05-05 00:50:45 +0800

    兼容 2016 以前

--- src/template/jinhua.ts
+++ src/template/jinhua.ts
@@ -17,12 +17,12 @@ import xlsx from 'node-xlsx';
 
 const DATA_CACHE_PATH = path.join(__dirname, '../data');
 const SOURCE_URL = 'http://www.jinhua.gov.cn/zjjh/jhnj/'
-
   ; (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(SOURCE_URL, {
-      waitUntil: 'networkidle2'
+      waitUntil: 'networkidle2',
+      timeout: 60 * 60 * 1000
     });
     const sourceList = await page.$$eval('.jh_gl_mr_m2 ul>li a', (aList: HTMLAnchorElement[]) => aList.map(aTag => ({
       title: aTag.innerText.trim(),
@@ -30,7 +30,8 @@ const SOURCE_URL = 'http://www.jinhua.gov.cn/zjjh/jhnj/'
     })));
     for (const source of sourceList) {
       await page.goto(source.target, {
-        waitUntil: 'networkidle2'
+        waitUntil: 'networkidle2',
+        timeout: 60 * 60 * 1000
       });
       const targetTable = await page.$$('table.MsoNormalTable, table#Table1');
       if (!!targetTable.length) {
@@ -55,7 +56,8 @@ const SOURCE_URL = 'http://www.jinhua.gov.cn/zjjh/jhnj/'
         for (const secondItem of secondPage) {
           for (const secondContent of secondItem.content) {
             await page.goto(secondContent.target, {
-              waitUntil: 'networkidle2'
+              waitUntil: 'networkidle2',
+              timeout: 60 * 60 * 1000
             });
             const tableHTML = await page.$eval('table', (table: HTMLElement) => table.outerHTML);
             // console.log(tableHTML);
@@ -69,24 +71,74 @@ const SOURCE_URL = 'http://www.jinhua.gov.cn/zjjh/jhnj/'
             }]);
             await fse.ensureDir(DATA_CACHE_PATH);
             await fse.writeFile(path.join(DATA_CACHE_PATH, `${source.title}-${secondItem.title}-${secondContent.title}.xlsx`), buffer);
-            // TODO: 这里为了节省流量，循环到第一个自动退出
-            break;
+            // // TODO: 这里为了节省流量，循环到第一个自动退出
+            // break;
           }
-          // TODO: 这里为了节省流量，循环到第一个自动退出
-          break;
+          // // TODO: 这里为了节省流量，循环到第一个自动退出
+          // break;
         }
       } else {
-        // TODO: 金华 2016 年及以前的数据需要点击 点击跳转 后才能正确展示
-        const redirectTarget = await page.$$eval('a', (aTagList: HTMLAnchorElement[]) => aTagList.find(aTag => (aTag.innerText.includes('点击阅读')) || { href: null } ).href);
+        // // TODO: 金华 2016 年及以前的数据需要点击 点击跳转 后才能正确展示
+        const redirectTarget = await page.$$eval('a', (aTagList: HTMLAnchorElement[]) => (aTagList.find(aTag => aTag.innerText.includes('点击阅览') || aTag.innerText.includes('点击浏览')) || { href: null }).href);
         if (redirectTarget) {
           await page.goto(redirectTarget, {
-            waitUntil: 'networkidle2'
+            waitUntil: 'networkidle2',
+            timeout: 60 * 60 * 1000
           });
+          const secondPage = await page.$$eval('table', (tableList: HTMLTableElement[]) => {
+            const targetTable = tableList.find(table => table.hasAttribute('id'));
+            const trList = Array.from(targetTable.querySelectorAll('tr'));
+            return trList.reduce<{ title: string, content: { title: string, target: string }[] }[]>((acc, current, index) => {
+              const subItems = Array.from(current.querySelectorAll('a'));
+              if (subItems.length) {
+                const prev = acc.pop() || { title: '', content: [] };
+                acc = [...acc, {
+                  ...prev,
+                  content: prev.content.concat(subItems.map(aTag => ({ title: aTag.innerText, target: aTag.href })))
+                }];
+              } else {
+                acc = [...acc, {
+                  title: current.innerText.trim(),
+                  content: []
+                }]
+              }
+              return acc;
+            }, []);
+          })
+          for (const secondItem of secondPage) {
+            for (const secondContent of secondItem.content) {
+              await page.goto(secondContent.target, {
+                waitUntil: 'networkidle2',
+                timeout: 60 * 60 * 1000
+              });
+              const targetTable = page.$('table')
+              if (targetTable) {
+                const tableHTML = await page.$eval('table', (table: HTMLElement) => table.outerHTML);
+                const result: { [x: number]: string }[][] = tabletojson.convert(tableHTML);
+                const resultNew = result.length ? result
+                  .map(item => (item.map(v => Object.keys(v).reduce((acc, k) => (acc[k] = v[k], acc), []))))
+                  .reduce((acc, v) => [...acc, ...v]) : []
+                const buffer = xlsx.build([{
+                  name: '表格',
+                  data: resultNew
+                }]);
+                await fse.ensureDir(DATA_CACHE_PATH);
+                await fse.writeFile(path.join(DATA_CACHE_PATH, `${source.title}-${secondItem.title}-${secondContent.title}.xlsx`), buffer);
+              } else {
+                // page.$
+                console.log('img --->', secondContent.target)
+              }
+              // // TODO: 这里为了节省流量，循环到第一个自动退出
+              // break;
+            }
+            // // TODO: 这里为了节省流量，循环到第一个自动退出
+            // break;
+          }
         } else {
-          console.log(source.target)
+          console.log('ccc -->', source.target)
         }
       }
-      // TODO: 这里为了节省流量，循环到第一个自动退出
+      // // TODO: 这里为了节省流量，循环到第一个自动退出
       // break;
     }
     // console.log(result);
