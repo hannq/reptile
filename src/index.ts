9ff9b6c2a6bb304e80012148d8b135cbe52eee45
hannq <1094489295@qq.com>
2020-05-06 09:13:21 +0800

    add electron

--- src/index.ts
+++ src/index.ts
@@ -1,83 +1,42 @@
-/*
- * @Author: hannq
- * @Date: 2020-04-29 00:09:34
- * @Last Modified by: hannq
- * @Last Modified time: 2020-05-01 00:05:30
- * @desc 主入口文件
- */
-
-import puppeteer from 'puppeteer';
-import Fuse from 'fuse.js';
+import { app, BrowserWindow } from 'electron';
 import path from 'path';
-import micromatch from 'micromatch';
-import fse from 'fs-extra';
-import { Tabletojson as tabletojson } from 'tabletojson';
-import xlsx from 'node-xlsx';
 
-const DATA_CACHE_PATH = path.join(__dirname, './data');
-
-;(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('http://tjj.huzhou.gov.cn/tjsj/tjnj/index.html', {
-    waitUntil: 'networkidle2'
-  });
-  const listText = await page.$$eval('ul#ajaxpage-list a', (aList: HTMLAnchorElement[]) => aList.map(e => ({title: e.innerText, target: e.href})));;
-  for(const item of listText) {
-    await page.goto(item.target, {
-      waitUntil: 'networkidle2'
-    });
-    const secondPage = await page.$$eval('.TRS_Editor a', (aList: HTMLAnchorElement[], item) => {
-      const aTag = aList.find(e => e.innerText === '点击查看详细信息');
-      return {
-        referer: item,
-        title: aTag.innerText,
-        target: aTag.href,
-      }
-    }, item);
-    await page.goto(secondPage.target, {
-      waitUntil: 'networkidle2'
-    });
-    const thridPage = await page.$$eval('table tr a', (aList: HTMLAnchorElement[], item) => {
-      return aList.map(val => ({
-        referer: item,
-        title: val.innerText,
-        target: val.href
-      }))
-    }, item);
-    for (const item of thridPage) {
-      await page.goto(item.target, {
-        waitUntil: 'networkidle2'
-      });
-      const forthPage = await page.$$eval('table tr a', (aList: HTMLAnchorElement[], item) => {
-        return aList.map(val => ({
-          referer: item,
-          title: val.innerText,
-          target: val.href
-        }))
-      }, item);
-      console.log(forthPage);
-      for (const item of forthPage) {
-        await page.goto(item.target, {
-          waitUntil: 'networkidle2'
-        });
-        const tableText = await page.$eval('table', (table: HTMLElement) => table.outerHTML);
-        console.log(tableText)
-        const result: {[x: number]: string}[][] = tabletojson.convert(tableText);
-        console.log(result)
-        await fse.writeFile('tabel.html', tableText)
-        const resultNew = result.length ? result
-          .map(item => (item.map(v => (console.log(v), Object.keys(v).reduce((acc, k) => (acc[k] = v[k], acc), [])))))
-          .reduce((acc, v) => [...acc, ...v]) : []
-        const buffer = xlsx.build([{
-          name: '表格',
-          data: resultNew
-        }]);
-        await fse.ensureDir(DATA_CACHE_PATH);
-        await fse.writeFile(path.join(DATA_CACHE_PATH, `${item.referer.referer.title}-${item.referer.title}-${item.title}.xlsx`), buffer)
-      }
+function createWindow () {
+  // 创建浏览器窗口
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
     }
+  })
 
+  // 并且为你的应用加载index.html
+  win.loadFile(path.join(__dirname, '../index.html'))
+
+  // 打开开发者工具
+  win.webContents.openDevTools()
+  require('devtron').install()
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// 部分 API 在 ready 事件触发后才能使用。
+app.whenReady().then(createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // 在 macOS 上，除非用户用 Cmd + Q 确定地退出，
+  // 否则绝大部分应用及其菜单栏会保持激活。
+  if (process.platform !== 'darwin') {
+    app.quit()
   }
-  await browser.close();
-})();
+})
+
+app.on('activate', () => {
+  // 在macOS上，当单击dock图标并且没有其他窗口打开时，
+  // 通常在应用程序中重新创建一个窗口。
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
